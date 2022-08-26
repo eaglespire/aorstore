78f5c122abf88561460d0b5c53be435864d7e273
eaglespire <eaglespiresolutions@gmail.com>
2022-08-26 04:24:32 +0100

    themes added

---
+++ themes/vertical-dark/assets/pages/email-summernote.init.js
@@ -0,0 +1,14 @@
+/*
+ Template Name: Veltrix - Responsive Bootstrap 4 Admin Dashboard
+ Author: Themesbrand
+ File: Email summernote Init
+*/
+
+$(document).ready(function(){
+    $('.summernote').summernote({
+        height: 250,                 // set editor height
+        minHeight: null,             // set minimum height of editor
+        maxHeight: null,             // set maximum height of editor
+        focus: false                 // set focus to editable area after initializing summernote
+    });
+});
---
+++ themes/vertical-dark/assets/pages/knob.init.js
@@ -0,0 +1,10 @@
+/*
+ Template Name: Veltrix - Responsive Bootstrap 4 Admin Dashboard
+ Author: Themesbrand
+ File: Knob Init
+*/
+
+$(function() {
+    $(".knob").knob({
+    });
+});
